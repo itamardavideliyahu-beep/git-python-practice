15ab936740fa4cb11b0d369f4f7e9ddf62f8205c
Itamar <itamardavideliyahu@gmail.com>
2026-01-18 00:22:01 +0200

    adding some lines..

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ import datetime
 def greet(name):
     return f"Hello from {name}!"
     return f"Hello baby, {name}!"
-
+    return f"Hello father, {name}!"
+    return f"Hello mother, {name}!"
+    return f"Hello sister, {name}!"
+    return f"Hello brother, {name}!"
+    return f"Hello uncle, {name}!"
+    return f"Hello aunt, {name}!"
+    return f"Hello cousin, {name}!"
+    return f"Hello friend, {name}!"
+    return f"Hello enemy, {name}!"
 
 if __name__ == "__main__":
     user = "World"
