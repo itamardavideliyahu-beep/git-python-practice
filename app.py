7408ddca9073338f381204d670b72a00cc9ca3d7
Itamar <itamardavideliyahu@gmail.com>
2026-01-22 22:57:10 +0200

    frebb

--- app.py
+++ app.py
@@ -1,45 +1,15 @@
 import datetime
-import socket
+
 
 def greet(name):
     return f"Hello from {name}!"
-    return f"Hello baby, {name}!"
-    return f"Hello father, {name}!"
-    return f"Hello mother, {name}!"
-    return f"Hello sister, {name}!"
-    return f"Hello brother, {name}!"
-    return f"Hello uncle, {name}!"
-    return f"Hello aunt, {name}!"
-    return f"Hello cousin, {name}!"
-    return f"Hello friend, {name}!"
-    return f"Hello enemy, {name}!"
-    return f"{name} is a good boy!"
-    
+
 
 if __name__ == "__main__":
     user = "World"
     print(greet(user), datetime.datetime.now())
-#44
-#33
-#44
-#55
-
-#55
-
-
-#1
-
-
-
-
-#2
-
-
-
-
 
 if __name__ == "__main__":
     user = "World"
-    print(greet(user), datetime.datetime.now()))
-#55
+    print(greet(user), datetime.datetime.now())
 
